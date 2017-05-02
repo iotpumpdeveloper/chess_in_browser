25f5d03da252f1fd06bf15f3a4b8e3bcb4f7b9b8
jim zike huang <iotpump.developer@gmail.com>
2017-05-01 23:01:05 -0700

    allow dynamic component mapping between tag and component

--- src/components/ChessPortal/script.js
+++ src/components/ChessPortal/script.js
@@ -1,9 +1,9 @@
 export default {
-  components : { //register component locally
-    'chessboard' : require('ChessBoard/component.vue'), //one line require local components!!!
-    'chess-status' : require('ChessStatus/component.vue'),
-    'chess-controlpanel' : require('ChessControlPanel/component.vue'),
-    'saved-games' : require('SavedGames/component.vue'),
+  componentMap : { //register component locally
+    'chessboard' : 'ChessBoard',
+    'chess-status' : 'ChessStatus',
+    'chess-controlpanel' : 'ChessControlPanel',
+    'saved-games' : 'SavedGames',
   },
 
   data () {
@@ -11,4 +11,24 @@ export default {
       'welcome_message' : 'Welcome to the chess game!',
     }
   },
+
+  beforeCreate () {
+    var componentImports = [];
+    var componentTagNames = [];
+    for (var tagName in this.$options.__proto__.componentMap) {
+      this.$options.__proto__.components[tagName] = {render: (h) => {}}; //by default no render function
+      var compName = this.$options.__proto__.componentMap[tagName];
+      componentTagNames.push(tagName);
+      componentImports.push(System.import('../' + compName + '/component.vue'));
+    }
+
+    Promise.all(componentImports).then( (modules) => {
+      for (var i = 0; i < modules.length; i ++ ) {
+        var tagName = componentTagNames[i];
+        this.$options.__proto__.components[tagName] = modules[i];
+        this.$forceUpdate();
+      }
+    });
+  }
+
 }
